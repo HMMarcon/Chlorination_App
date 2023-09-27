65006d4c6d90ad028a1a7be358b6d479ba024db2
HMMarcon <94055184+HMMarcon@users.noreply.github.com>
2023-09-27 11:00:40 +0100

    Update Chlorination_web-app-v2.py
    Removed the tutorial molecule. Now it will show as the standard on st_ketcher. User can modify it

--- Chlorination_web-app-v2.py
+++ Chlorination_web-app-v2.py
@@ -293,14 +293,10 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown('Enter the SMILES string below or select a molecule from our validation set:')
-    input_type = st.radio("Select your input method:", ("Tutorial molecule", "Draw your molecule", "SMILES string", "SDF file"))
+    input_type = st.radio("Select your input method:", ("Draw your molecule", "SMILES string", "SDF file"))
 
-    if input_type == "Tutorial molecule":
-        st.write("Tutorial molecule")
-        smiles_string = ("CC1=CC=CC2=C1C=CC=C2")
-
-    elif input_type == "Draw your molecule":
-        smiles_string = st_ketcher()
+    if input_type == "Draw your molecule":
+        smiles_string = st_ketcher("CC1=CC=CC2=C1C=CC=C2")
 
     elif input_type == "SMILES string":
 
