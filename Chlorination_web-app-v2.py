084ddb5b3601062f367dd52e6ee1c70a85a6a729
HMMarcon <hmm59@cam.ac.uk>
2024-02-19 16:21:09 +0000

    Update Chlorination_web-app-v2.py
    Added:
    - Error-handling
    - Progress bar for multiple halogenations

--- Chlorination_web-app-v2.py
+++ Chlorination_web-app-v2.py
@@ -4,12 +4,9 @@ st.set_page_config(layout="wide")
 
 # Data loading and management
 import pandas as pd
-import numpy as np
-from tqdm import tqdm
 import pickle
-
 import numpy as np
-from scipy.stats import norm
+
 
 # Basic chemistry packages
 from rdkit import Chem
@@ -28,14 +25,7 @@ from rdkit.Chem import rdDepictor
 IPythonConsole.ipython_useSVG = True
 rdDepictor.SetPreferCoordGen(True)
 
-# Data pre-processing packages
-from sklearn.decomposition import TruncatedSVD
-from sklearn.model_selection import train_test_split
 
-# Regression Analysis
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.linear_model import Ridge
-from sklearn.tree import DecisionTreeRegressor
 
 # Non-standard metrics
 from sklearn.metrics import matthews_corrcoef as MCC
@@ -46,34 +36,9 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({"figure.dpi": 96})
 plt_parameters = plt.rcParams
 plt.rcParams.update(plt_parameters)  # to reset parameters back to plt from sns
-
-# For interacting with molecules as widgets
-# from ipywidgets import interact
-# import ipywidgets as widgets
-
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import ElasticNet
-from xgboost import XGBClassifier
-from xgboost import XGBRegressor
-from sklearn.tree import DecisionTreeClassifier
-
-from sklearn.model_selection import cross_validate
 import seaborn as sns
 
 sns.set_theme(style="ticks")
-from sklearn.metrics import make_scorer
-
-from sklearn.tree import ExtraTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingRegressor
-from sklearn.tree import ExtraTreeRegressor
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import BayesianRidge
 
 # Fingerprints functions
 fp_size = [2048]
@@ -101,8 +66,6 @@ models_FP2FP = {}
 
 models_SVD2SVD = {}
 
-
-# X_validation = pd.DataFrame("") #Validation set
 
 def possible_prods(starting_material):
     """
@@ -174,37 +137,15 @@ def compare_FP_to_products_MCC(pred_FP, products):
 
     return prods_FP[most_similar], similarity_results[most_similar], products[most_similar]
 
-
-
 def load_models_old():
     return pickle.load(open("model_fp2fp_Ridge_7.5.mlpickle", "rb"))
 @st.cache_resource
 def load_models():
     return pickle.load(open("superset_model.pkl", "rb"))
 
-# @st.cache_resource
-# def load_svd():
-#    return pickle.load(open("svd_transform.p", "rb"))
-
-# svd2svd_models = load_models()
-# svd = load_svd()
 fp2fp_model = load_models()
 
-def modified_z_score(data):
-    """
-    Calculates the modified Z-score for each observation in the data.
-    """
-    median = np.median(data)
-    mad = np.median(np.abs(data - median))
-    modified_z = 0.6745 * (data - median) / mad
-    return modified_z
 
-def z_score_to_probability(z_score):
-    """
-    Converts a Z-score to a probability of being an outlier from the standard normal distribution.
-    """
-    probability = 1 - norm.sf(abs(z_score)) * 2
-    return probability
 
 def run_model_fp(model_key, mol):
     if model_key == "Your Choice":
@@ -217,68 +158,14 @@ def run_model_fp(model_key, mol):
         predictions_FP = model.predict(reactant_FP)[0]
         if len(np.unique(predictions_FP)) != 2:
             predictions_FP = np.digitize(predictions_FP, bins=[0.5, 3])
-        #print(predictions_FP)
+
 
         predicted_product = compare_FP_to_products_MCC(predictions_FP, possible_prods(
             Chem.MolFromSmiles(reactant)))[2]
-        predicted_product_FP = compare_FP_to_products_MCC(predictions_FP, possible_prods(
-            Chem.MolFromSmiles(reactant)))[0]
-        prods_FP = smiles_to_fp(possible_prods(Chem.MolFromSmiles(reactant)))
-        # prods_bit = [DataStructs.cDataStructs.CreateFromBitString("".join(prod_FP.astype(str))) for prod_FP in prods_FP]
-        similarity_mcc = []
-        similarity = []
 
-        #if len(prods_FP) == 1:
-        #    similarity = [MCC(prods_FP, predictions_FP)]
-
-        #elif len(prods_FP) == 2:
-            #Normalised MCC for two products. No real statistical meaning, just shows how much distribution favours one.
-        #    for prod_FP in prods_FP:
-        #        similarity.append(MCC(prod_FP, predictions_FP))
-            #similarity = similarity / sum(similarity)
-        #elif len(prods_FP) > 2:
-
-        #    for prod_FP in prods_FP:
-                ## Change the similarity metric to MCC between fingerprints
-        #        similarity_mcc.append(MCC(prod_FP, predictions_FP))
-                #similarity = (similarity - sum(similarity)/len(similarity)) / (max(similarity) - min(similarity))
-            #post-process similarity metrics to be between 0 and 1
-
-            #Should just do a Student's t-test? Sure!
-            #Former idea of a similarity metric based on a modified Z-score. Not used anymore.
-        #    #similarity_mod_Z = modified_z_score(similarity_mcc)
-        #    #similarity_proba = z_score_to_probability(similarity_mod_Z)
-        #    #similarity = similarity_proba
-        #    #similarity = (similarity - min(similarity)) / (max(similarity) - min(similarity))
-        #    #similarity = similarity * similarity_mcc
-
-
-        #for mcc_base in similarity_mcc:
-        #    index = similarity_mcc.index(mcc_base)
-        #    st.write(MCC(prods_FP[index],predicted_product_FP))
-        #   st.write(mcc_base)
-        #    similarity.append(mcc_base / MCC(prods_FP[index],predicted_product_FP))
 
         return [predicted_product]#, similarity
 
-
-def run_model_svd(model_key, mol):
-    if model_key == "Your Choice":
-        return selected_product
-    else:
-        model = svd2svd_models[model_key]
-
-        reactant = Chem.MolToSmiles(mol)
-        reactant_FP = smiles_to_fp([reactant])
-        reactant_svd = svd.transform(reactant_FP)
-        predictions_svd = model.predict(reactant_svd)[0]
-        predictions_FP = svd.inverse_transform([predictions_svd])
-        if len(np.unique(predictions_FP)) != 2:
-            predictions_FP = np.digitize(predictions_FP, bins=[0.5, 3])
-
-        predicted_product = compare_FP_to_products_MCC(predictions_FP[0], possible_prods(Chem.MolFromSmiles(reactant)))[
-            2]
-        return predicted_product
 
 intro_col1, intro_col2 = st.columns(2)
 
@@ -292,12 +179,6 @@ with intro_col2:
                 "To see it working, input the SMILES of the molecule you are interested in, then run the model.")
 
 st.image("workflow.png", use_column_width=True)
-
-
-
-#st.markdown("It also give a confidence score for the prediction, which is based on the predicted FP similarity "
-#            "and the similarity distribution within the range of possible products."
-#            "its distribution among different possible products")
 
 st.markdown("Disclaimer: No data is stored in this version of the app.")
 st.markdown("----------------------------------------------------------------")
@@ -327,22 +208,29 @@ with col1:
 
     mol = Chem.MolFromSmiles(smiles_string)
 
-    if mol is  None:
-        st.text('Invalid SMILES string')
+    try:
+        mol = Chem.MolFromSmiles(smiles_string)
 
-    show_products_bar = False
+        if mol is None:
+            raise ValueError("Invalid Molecule")
+        elif len(smiles_string) == 0:
+            raise ValueError("No molecule found")
+        elif count_halogenations(mol) == 0:
+            raise ValueError("No aromatic substitution site found in molecule")
 
-    # show_products = st.button("Show possible products")
+    except ValueError as e:
+        st.error(str(e))
+        st.stop()
+
 
 with col2:
-    # col11, col22 = st.columns(2)
 
-    # with col11:
-    # Show possible products
     st.markdown("#### Predicted chlorination product")
 
-
-    nb_halogens = st.select_slider("Number of halogenations", options=list(range(1, count_halogenations(mol)+1)), value=1)
+    if count_halogenations(mol) > 1:
+        nb_halogens = st.select_slider("Number of halogenations", options=list(range(1, count_halogenations(mol)+1)), value=1)
+    else:
+        nb_halogens = 1
     st.write("Number of halogenations: ", nb_halogens)
     products = []
     products_image = []
@@ -351,6 +239,7 @@ with col2:
     potential_products = [Chem.MolToSmiles(mol)]
     products_list = [mol]
 
+    progress_bar = st.progress(0)
     for i in range(nb_halogens):
         products = []
         for potential_product in potential_products:
@@ -360,7 +249,7 @@ with col2:
         potential_products = products
         #Here!
         products_list.append(Chem.MolFromSmiles(run_model_fp("Ridge", products_list[i])[0]))
-
+        progress_bar.progress((i+1)/nb_halogens, "Progress: " + str(i+1) + "/" + str(nb_halogens) + " halogenations")
     predicted_product = products_list[-1]
 
 
@@ -378,17 +267,11 @@ with col2:
         products_image.append(Draw.MolToImage(potential_product, size=(100, 100)))
 
 
-
-    # st.text('Possible products:')
-
-
-
     predictions = {"ML prediction": predicted_product}
-    # similarity = run_model_fp("Ridge", mol)[1]
-    # selected_product = st.radio("Select the major product:", options=range(len(products)), on_change=None)
+
     st.image(Draw.MolToImage(predictions["ML prediction"]))
     st.markdown("Product SMILES: " + Chem.MolToSmiles(predicted_product))
-    # st.markdown("Confidence score: " + str(round(1 + max(similarity), 4) - 1))
+
 
     model_selection = []
 
@@ -403,17 +286,9 @@ with col2:
                 model_selection.append("")
 
 
-    #for potential_product in potential_products:
-    #    products.append(Chem.MolFromSmiles(potential_product))
-    #    products_image.append(Draw.MolToImage(Chem.MolFromSmiles(potential_product), size=(100, 100)))
-
     if len(potential_products) == 0:
-        st.text('No possible products found')
-    # else:
-    #    st.image(products_image)
+        raise ValueError('No possible products found')
 
-
-    run_models = st.button("Refresh model output")
 
 
 rxn_string = Chem.MolToSmiles(mol)
@@ -423,19 +298,7 @@ for molecule in products_list[1:]:
 st.markdown("#### Reaction scheme")
 rxn_schema = st.columns([0.5, 2*len(products_list), 0.5])[1]
 with rxn_schema:
-    st.image(Draw.MolsToImage(products_list, molsPerRow=len(products_list), subImgSize=(250, 250)), use_column_width=True)
-# predictions = {"Your Choice": products[selected_product],
-#               "Ridge": Chem.MolFromSmiles(run_model_svd("Ridge", mol)),
-#               "Ridge_10": Chem.MolFromSmiles(run_model_svd("Ridge_10", mol)),
-#               "Ridge_50": Chem.MolFromSmiles(run_model_svd("Ridge_50", mol)),
-#               "Lasso": Chem.MolFromSmiles(run_model_svd("Lasso", mol)),
-#               "Elastic Net": Chem.MolFromSmiles(run_model_svd("Elastic Net", mol)),
-#               "BayesianRidge": Chem.MolFromSmiles(run_model_svd("BayesianRidge", mol)),
-#               "DecisionTreeRegressor": Chem.MolFromSmiles(run_model_svd("DecisionTreeRegressor", mol)),
-#               "ExtraTreeRegressor": Chem.MolFromSmiles(run_model_svd("ExtraTreeRegressor", mol)),
-#               "XGBoostRegressor": Chem.MolFromSmiles(run_model_svd("XGBoostRegressor", mol)),
-#               "AdaBoostRegressor": Chem.MolFromSmiles(run_model_svd("AdaBoostRegressor", mol))
-#               }
+    st.image(Draw.MolsToImage(products_list, molsPerRow=len(products_list), subImgSize=(250, 250)), use_column_width=False)
 
 
 # Model outputs!
@@ -445,36 +308,30 @@ st.markdown("### Possible products list")
 
 col1, col2, col3 = st.columns(3)
 
-  ##Use here an empty list that grows inside the for loop
-
-
-
-
 data = {'Text': [f'Product  {i+1}' for i in range(len(products))],
         'Product': products_image,
         'Models': model_selection
-        #'Similarity': similarity
         }
 
 df = pd.DataFrame(data)
 
 for index, row in df.iterrows():
-    # st.write(model_selection[index])
+
     if index % 3 == 0:
         with col1:
             st.write(row['Text'])
             st.image(row['Product'], use_column_width=False)
             st.write(row['Models'])
-            #st.write("Confidence: ", row['Similarity'])
+
     elif index % 3 == 1:
         with col2:
             st.write(row['Text'])
             st.image(row['Product'], use_column_width=False)
             st.write(row['Models'])
-            #st.write("Confidence: ", row['Similarity'])
+
     elif index % 3 == 2:
         with col3:
             st.write(row['Text'])
             st.image(row['Product'], use_column_width=False)
             st.write(row['Models'])
-            #st.write("Confidence: ", row['Similarity'])
+
